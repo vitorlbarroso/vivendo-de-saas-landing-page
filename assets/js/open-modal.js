90b36d07ad02f5826aae3e538067b07041548839
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-07-27 03:41:20 -0300

    Adicionado o javascript do Modal

--- assets/js/open-modal.js
+++ assets/js/open-modal.js
@@ -1,10 +1,23 @@
+/* Pegando o Modal */
+const modal = document.getElementById('modal-form');
+
 /* Pegando os botões */
 const buttonHeader = document.getElementById('button-header');
 const buttonExplication = document.getElementById('button-explication');
+const buttonCloseModal = document.getElementById('close-modal');
+
+/* Função que fecha o Modal */
+const closeModal = () => {
+    modal.classList.remove('d-flex');
+    modal.classList.add('disabled');
+    buttonCloseModal.removeEventListener('click', closeModal);
+}
 
 /* Função que abre o Modal */
 const openModal = () => {
-    console.log('oi');
+    modal.classList.remove('disabled');
+    modal.classList.add('d-flex');
+    buttonCloseModal.addEventListener('click', closeModal);
 }
 
 /* Eventos que verificam o click */
